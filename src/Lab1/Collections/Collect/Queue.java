da2162e16bae954cd6db1d3d32ff87c84306ebe5
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2022-09-10 15:29:23 +0300

    Test push1

--- src/Lab1/Collections/Collect/Queue.java
+++ src/Lab1/Collections/Collect/Queue.java
@@ -1,5 +1,5 @@
 package Lab1.Collections.Collect;
-
+//Comment
 public interface Queue<T> extends Collection<T> {
     Queue<T> PushBack(T v) throws OversizeException;
     T peekBack() throws EmptyException, NullPointerException;
